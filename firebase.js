ac45bdb30a221fcfd5931662c2d7572294808c48
disamze <disamaze@gmail.com>
2025-05-23 11:48:49 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyANUNTnm11bHwXq4gBIiSaZkRdRO1SRy8E",
-    authDomain: "whatsapp-beb52.firebaseapp.com",
-    projectId: "whatsapp-beb52",
-    storageBucket: "whatsapp-beb52.firebasestorage.app",
-    messagingSenderId: "507807907595",
-    appId: "1:507807907595:web:c2ceb97eea274a05e2ac0e",
-    measurementId: "G-X3K4E91PFT"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 // Initialize Firebase
